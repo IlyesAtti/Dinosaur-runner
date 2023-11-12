cfb0049dc6455dfaf3a2dc33bca929863af2c0ed
Ilyes Attila <attis.ilyes@gmail.com>
2023-11-12 14:06:45 +0200

    1.1

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 document.onkeydown = detectKey;
 let score = 0;
-let obstaclesLeft = 100;
+let obstaclesLeft = 770;
 let stop = 0;
 let fallTime = 1000;
+let obstacleSpeed = 10;
+
+function start() {
+    document.getElementById("start").style= "visibility: hidden";
+    document.getElementById("dinosasur").style = "visibility: visibil";
+    document.getElementById("obstacles").style = "visibility: visibil";
+    setInterval(increaseScore, 100); //  -->  0,1 sec
+    setInterval(moveObstacle, obstacleSpeed);
+    dinoFall();
+}
 
 function detectKey(e) {
     e = e || window.event;
@@ -16,19 +26,10 @@ function increaseScore() {
     if (stop == 0) {
         ++score;
         document.getElementById("score").innerHTML = "Score: " + score;
-        moveObstacle();
-        console.log(fallTime);
-        if (score % 200 == 0 && fallTime > 100) {
-            speedUp();
+        if (score % 200 == 0 && fallTime > 100) { 
+            speedUp();  // every 20 sec
         }
     }
-}
-
-function start() {
-    document.getElementById("start").style= "visibility: hidden";
-    document.getElementById("dinosasur").style = "visibility: visibil";
-    document.getElementById("obstacles").style = "visibility: visibil";
-    setInterval(increaseScore, 100);
 }
 
 function dinoFall() {
@@ -37,12 +38,12 @@ function dinoFall() {
 
 function moveObstacle() {
     if (stop == 0) {
-        obstacles.style.left = (obstaclesLeft) + "%";
+        obstacles.style.left = (obstaclesLeft) + "px";
         obstaclesLeft -= 1;
         if (obstaclesLeft < 0) {
-            obstaclesLeft = 100;
+            obstaclesLeft = 770;
         }
-        if (obstaclesLeft < 9 && obstaclesLeft > 1) {
+        if (obstaclesLeft < 67 && obstaclesLeft > 11) {
             if ( dinosasur.style.top == "75%") {
                 stop = 1;
             }
@@ -51,6 +52,6 @@ function moveObstacle() {
 }
 
 function speedUp() {
-    setInterval (moveObstacle, 100);
+    setInterval (moveObstacle, 10);
     fallTime -= 100;
 }
